46987a94d3b2b787020d83fec5b7eebdc834a84b
Michel <michel.bolzon123@gmail.com>
2018-05-20 18:22:03 -0300

    final

--- src/trabalhopalavras/TrabalhoPalavras.java
+++ src/trabalhopalavras/TrabalhoPalavras.java
@@ -72,8 +72,8 @@ public class TrabalhoPalavras {
             /**/System.out.println("Exemplo 3:");
             /**/System.out.println("Nro Vertices: "+ grafo.getVertice().size());                                                        /**/
             /**/System.out.println("Nro Arestas: "+ grafo.getAresta().size());
-            /**/System.out.println("Caminho de "+grafo.getVertice().get(3)+" até "+grafo.getVertice().get(5)+" é: "+                  /**/
-            /**/Algoritmos.getCaminho(grafo, grafo.getVertice().get(3), grafo.getVertice().get(5)));                                  /**/
+            /**/System.out.println("Caminho de "+grafo.getVertice().get(191)+" até "+grafo.getVertice().get(33)+" é: "+                  /**/
+            /**/Algoritmos.getCaminho(grafo, grafo.getVertice().get(191), grafo.getVertice().get(33)));                                  /**/
             /**/grafo.limpaGrafo(grafo);                                                                                                /**/
             /**/System.out.println("Nro Componentes Conexos: "+ nroCompConexos);                                                        /**/
             /**/grafo.limpaGrafo(grafo);                                                                                                /**/
